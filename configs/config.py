ca8bd3e20863c0d7fcb18ce2e246ed269b209813
moko <no_moko@icloud.com>
2023-02-09 01:07:59 +0800

    remove comment

--- configs/config.py
+++ configs/config.py
@@ -15,36 +15,7 @@ def load_yml(file: Path, encoding="utf-8") -> dict:
 
 # 获取当前路径
 CONFIG_PATH = Path(os.getcwd()) / "configs" / "config.yml"
-print(CONFIG_PATH)
 config = ConfigsManager(CONFIG_PATH)
-
-# config = load_yml(CONFIG_PATH)
-
-
-# class BotSelfConfig:
-#     config: dict = config["BotSelfConfig"]
-#
-#     host: IPv4Address = IPv4Address(config.get("host", "127.0.0.1"))
-#     port: int = int(config.get("port", 8080))
-#     debug: bool = bool(config.get("debug", False))
-#     superusers: set = set(config.get("superusers", ["21778445"]))
-#     nickname: set = set(config.get("nickname", ["ATRI", "Atri", "atri", "亚托莉", "アトリ"]))
-#     command_start: set = set(config.get("command_start", ["", "/"]))
-#     command_sep: set = set(config.get("command_sep", ["."]))
-#     session_expire_timeout: timedelta = timedelta(
-#         seconds=config.get("session_expire_timeout", 60)
-#     )
-#     proxy: str = config.get("proxy", None)
-#
-#
-# class InlineGoCQHTTP:
-#     config: dict = config["InlineGoCQHTTP"]
-#
-#     enabled: bool = bool(config.get("enabled", True))
-#     accounts: list = config.get("accounts", [])
-#     download_domain: str = config.get("download_domain", "download.fastgit.org")
-#     download_version: str = str(config.get("download_version", "v1.0.0-rc1"))
-
 
 RUNTIME_CONFIG = {
     "host": config.get_config("BotSelfConfig", "host"),
