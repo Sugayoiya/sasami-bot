07453154e8cdfbf45242193ff9f0d8cd2c5372ed
moko <no_moko@icloud.com>
2023-03-10 00:37:25 +0800

    chatgpt

--- plugins/chatgpt/__init__.py
+++ plugins/chatgpt/__init__.py
@@ -1,26 +1,29 @@
-# from nonebot.adapters.onebot.v11 import MessageEvent
-# from nonebot.adapters.onebot.v11.bot import Bot
-# from nonebot.plugin import on_message
-# from nonebot.rule import to_me
-#
-# from utils.utils import cooldow_checker
-# from .chatGPT_v1 import ChatGPT
-#
-# __plugin_name__ = "chatgpt"
-#
-# __plugin_usage__ = """
-# usage：
-#     chatgpt
-# """.strip()
-#
-# chatgpt = on_message(priority=98, block=False, rule=to_me())
-# chat_bot = ChatGPT()
-#
-#
-# @chatgpt.handle(parameterless=[cooldow_checker("chatgpt", 30)])
-# async def _(bot: Bot, event: MessageEvent):
-#     # 不响应自身发送的消息
-#     if event.sender.user_id == event.self_id:
-#         return
-#     res = await chat_bot.ask(event.get_plaintext())
-#     await chatgpt.finish(res)
+from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.plugin import on_message
+from nonebot.rule import to_me
+
+from configs.path_config import TEXT_PATH
+from utils.utils import cooldow_checker
+from .chatGPT import Chatbot
+
+__plugin_name__ = "chatgpt"
+
+__plugin_usage__ = """
+usage：
+    chatgpt
+""".strip()
+
+path = TEXT_PATH / "chatgpt" / "config.json"
+chatgpt = on_message(priority=98, block=False, rule=to_me())
+chat_bot = Chatbot("")
+chat_bot.load_config(path)
+
+
+@chatgpt.handle()
+async def _(bot: Bot, event: MessageEvent):
+    # 不响应自身发送的消息
+    if event.sender.user_id == event.self_id:
+        return
+    res = chat_bot.ask_stream(event.get_plaintext())
+    await chatgpt.finish(res)
